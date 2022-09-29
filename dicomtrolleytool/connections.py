b624ac0f1a8600925c2e16375cc6b6a4f12011df
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2022-09-29 12:09:48 +0200

    Factors base components out of MintConnection

--- dicomtrolleytool/connections.py
+++ dicomtrolleytool/connections.py
@@ -1,5 +1,6 @@
 """Models things that can directly connect to a DICOM server"""
 import json
+from typing import List
 
 from dicomtrolleytool.persistence import Storage
 from pydantic.main import BaseModel
@@ -9,11 +10,25 @@ from dicomtrolley.auth import create_session
 from dicomtrolley.mint import Mint
 
 
-class Connection:
-    pass
+class Connection(BaseModel):
+    @classmethod
+    def init_from_storage(cls, storage: Storage, key: str):
+        """Init the connection stored under key"""
+        params = json.loads(storage.read(key))
+        return cls(**params)
+
+    def write_to_storage(self, storage: Storage, key: str):
+        """Write this connection to storage under key"""
+        params = self.dict()
+        for name in self.get_secret_param_names():
+            params[name] = getattr(self, name).get_secret_value()
+        storage.write(key, json.dumps(params))
+
+    def get_secret_param_names(self) -> List[str]:
+        return [name for name, f in self.__fields__.items() if f.type_ == SecretStr]
 
 
-class MintConnection(Connection, BaseModel):
+class MintConnection(Connection):
     """Can do DICOM searches with MINT
 
     Wrapper around dicomtrolley.
@@ -25,19 +40,7 @@ class MintConnection(Connection, BaseModel):
     password: SecretStr
     realm: str
 
-    @classmethod
-    def init_from_storage(cls, storage: Storage, key: str):
-        """Init the connection stored under key"""
-        params = json.loads(storage.read(key))
-        return cls(**params)
-
-    def write_to_storage(self, storage: Storage, key: str):
-        """Write this connection to storage under key"""
-        params = self.dict()
-        params["password"] = self.password.get_secret_value()
-        storage.write(key, json.dumps(params))
-
-    def init_searcher(self):
+    def init_searcher(self) -> Mint:
         """Create a searcher instance from this connection"""
         session = create_session(
             self.login_url, self.user, self.password.get_secret_value(), self.realm
